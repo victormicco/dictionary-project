fe3417f88d4849fc152649799d9a0b527f75d0a7
Victor Micco <victor.oficial093@gmail.com>
2023-08-23 17:54:32 -0300

    before api connection

---
+++ src/hooks/ThemeContext.jsx
@@ -0,0 +1,51 @@
+"use client"
+import React, {createContext, useEffect, useState} from "react";
+
+
+
+const getInitialTheme = () => {
+    if (typeof window !== "undefined" && window.localStorage) {
+        const storedPrefs = window.localStorage.getItem('color-theme');
+        if (typeof storedPrefs === "string") {
+            return storedPrefs;
+        }
+
+        const useMedia = window.matchMedia('(prefers-color-scheme: dark)');
+        if (useMedia.matches) {
+            return 'dark';
+        }
+}
+return 'light';
+};
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({initialTheme, children}) => {
+    const [theme, setTheme] = useState(getInitialTheme) 
+
+    const rawSetTheme = (rawTheme) => {
+        const root = window.document.documentElement;
+        const isDark = rawTheme === 'dark';
+
+        root.classList.remove(isDark ? 'light' : 'dark');
+        root.classList.add(rawTheme)
+
+        localStorage.setItem('color-theme', rawTheme);
+    }
+
+    if (initialTheme){
+        rawSetTheme(initialTheme);
+    }
+
+    useEffect(() => {
+       rawSetTheme(theme);
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+
+
+}
